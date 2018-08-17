195509be5918a90b9afe2c53329fcd4ea4b937e1
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-08-17 17:44:50 +0200

    Initial commit Project Skeleton Initial Integration Test

---
+++ src/main/java/net/adoptopenjdk/tests/parser/MainApplication.java
@@ -0,0 +1,30 @@
+package net.adoptopenjdk.tests.parser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class MainApplication implements ApplicationRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        LOGGER.info("Running AdoptOpenJDK CI Log Parser");
+
+        String strArgs = Arrays.stream(args.getSourceArgs()).collect(Collectors.joining("|"));
+        LOGGER.info("Application started with arguments:" + strArgs);
+    }
+}
---
+++ src/test/java/net/adoptopenjdk/tests/parser/MainApplicationTest.java
@@ -0,0 +1,80 @@
+package net.adoptopenjdk.tests.parser;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { MainApplication.class })
+public class MainApplicationTest {
+
+    @Autowired
+    private MainApplication application;
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
+    @Test
+    public void passingNoArgumentTest() throws Exception {
+
+        ApplicationArguments testArguments = new DefaultApplicationArguments(new String[] {});
+
+        application.run(testArguments);
+        assertThat(outputCapture.toString()).contains("NOT PARSED");
+    }
+
+    @Test
+    public void passingLocalFileTest() throws Exception {
+
+        ApplicationArguments testArguments = new DefaultApplicationArguments(new String[] {
+                "file:sample.log"
+        });
+
+        application.run(testArguments);
+        assertThat(outputCapture.toString()).contains("PARSED");
+    }
+
+    @Test
+    public void passingWebAddressTest() throws Exception {
+
+        ApplicationArguments testArguments = new DefaultApplicationArguments(new String[] {
+                "url:https://ci.adoptopenjdk.net/view/ev3dev/job/openjdk11_openjdktest_ev3_linux/7/consoleFull"
+        });
+
+        application.run(testArguments);
+        assertThat(outputCapture.toString()).contains("PARSED");
+    }
+
+    @Test
+    public void passingMultipleParameters() throws Exception {
+
+        ApplicationArguments testArguments = new DefaultApplicationArguments(new String[] {
+                "file:sample.log",
+                "file:sample2.log"
+        });
+
+        application.run(testArguments);
+        assertThat(outputCapture.toString()).contains("NOT PARSED");
+    }
+
+    @Test
+    public void passingMultipleMixedParameters() throws Exception {
+
+        ApplicationArguments testArguments = new DefaultApplicationArguments(new String[] {
+                "file:sample.log",
+                "url:https://ci.adoptopenjdk.net/view/ev3dev/job/openjdk11_openjdktest_ev3_linux/7/consoleFull"
+        });
+
+        application.run(testArguments);
+        assertThat(outputCapture.toString()).contains("NOT PARSED");
+    }
+
+}
